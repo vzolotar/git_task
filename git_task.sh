ad564ffe263096dfc4fc7efd01a588b459e30b17
Vlad Zolotarev <kruto3000@gmail.com>
2020-07-13 23:16:16 -0500

    initial commit

--- git_task.sh
+++ git_task.sh
@@ -1,9 +1,8 @@
 # Simple Git Push Automation
 #
 
-cd /home/su/GitHub/git_task
 git add .
-git commit -m 'initial commit'
-git push https://github.com/vzolotar/git_task.git
+git commit -m 'test commit'
+git push https://github.com/username/git_task.git
 
-# Last Updated - Mon Jul 13 23:14:11 CDT 2020
+# Last Updated - Mon Jul 13 23:16:16 CDT 2020
