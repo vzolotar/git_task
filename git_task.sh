5408caa6e0383c0aaf5dedbf466dd9272651d204
Vlad Zolotarev <vzolotar@users.noreply.github.com>
2022-01-11 17:01:02 -0500

    Update git_task.sh

--- git_task.sh
+++ git_task.sh
@@ -6,5 +6,5 @@ git add .
 git commit -m 'test commit'
 git push https://github.com/username/git_task.git
 
-# Last Updated - Sun Jan  9 11:10:01 EST 2022
+
 # Last Updated - Tue Jan 11 16:59:47 EST 2022
