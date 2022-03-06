17c01c0166de128272bede13de65ca9d3b615a41
Vlad Zolotarev <kruto3000@gmail.com>
2022-03-05 21:02:01 -0500

    initial commit

--- git_task.sh
+++ git_task.sh
@@ -4,4 +4,4 @@ git add .
 git commit -m 'test commit'
 git push https://github.com/username/git_task.git
 
-# Last Updated - Sat Mar  5 09:01:58 EST 2022
+# Last Updated - Sat Mar  5 21:02:01 EST 2022
